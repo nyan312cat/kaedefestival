cbea2f4aa1ad2d30f46e52bfe7e037abd468ba2f
unknown <midai312dm@gmail.com>
2020-04-14 21:59:47 +0900

    猫　commonjsの読み込み順を後に

--- htmlver/common.js
+++ htmlver/common.js
@@ -1,39 +1,16 @@
-(async ()=>{
+(()=>{
 	"use strict";
+	const oldest=2020,newest=2020;
 	const doc=document;
 	//各年度へのリンク作成・追加　始まり　（最新からスタート）
-	if(!doc.getElementById("eachYearMenu"))return loadEnd();
-	doc.cookie="test=test;max-age=60";
-	if(!doc.cookie.includes("test=test"))alert("cookieを有効化してください");
-	const cookie=(doc.cookie?doc.cookie.split("; "):[]).reduce((res,val)=>{
-		if(!/.+=.+/.test(val))return res;
-		val=val.split("=");
-		res[val[0]]=val[1];
-		return res;
-	},{});
-	let start,end;
-	if(cookie.start_year===undefined||cookie.end_year===undefined){
-		const db=firebase.firestore().collection("kaedefestival").orderBy("year"),
-			sQS=await db.limit(2).get(),eQS=await db.limitToLast(1).get();
-		if(sQS.empty||eQS.empty||!sQS.docs[1]||!eQS.docs[0])return loadEnd();
-		start=sQS.docs[1].data().year;
-		end=eQS.docs[0].data().year;
-		doc.cookie="start_year="+start+";path=/;max-age=60*60*24";
-		doc.cookie="end_year="+end+";path=/;max-age=60*60*24";
-	}else{
-		start=cookie.start_year;
-		end=cookie.end_year;
-	}
+	if(!doc.getElementById("eachYearMenu"))return loadEnd(newest);
 	const f=doc.createDocumentFragment();
-	for(let i=end;i>=start;i--){
+	for(let i=oldest;i<newest;i++){
 		const li=doc.createElement("li");
 		li.innerHTML="<a href='/eachYear.html?year="+i+"'>"+i+"年度</a>";
 		f.appendChild(li);
 	}
 	doc.getElementById("eachYearMenu").appendChild(f);
 	//各年度へのリンク作成・追加　終わり
-	const date=new Date()
-	date.setMonth(date.getMonth()-3);
-	window.nenndo=date.getFullYear();
-	return loadEnd();
+	return loadEnd(newest);
 })();
--- htmlver/local/updateSchedule.js
+++ htmlver/local/updateSchedule.js
@@ -37,65 +37,75 @@
       }
     }
   });
-  const db=firebase.firestore().doc("/kaedefestival/kaede"+nenndo);//本番で変更
-  document.getElementById("upload").addEventListener("click",()=>{
-    let error=false;
-    const doError=(key,i)=>{
-      error=true;
-      doc.getElementById("div_"+key).getElementsByTagName("input")[i].style.backgroundColor="rgba(255,0,0,0.5)";
-    };
-    for(const key in datas){
-      const data=datas[key],l=data.length;
-      for(let i=0;i<l;i++){
-        const v=data[i];
-        if(!v[0])doError(key,i*3);
-        if(!timeReg.test(v[1]))doError(key,i*3+1);
-        if(!timeReg.test(v[2]))doError(key,i*3+2);
-        const s=v[1].split(":"),e=v[2].split(":");
-        if(s[0]>24||s[1]>59)doError(key,i*3+1);
-        if(e[0]>24||e[1]>59)doError(key,i*3+2);
-        if((+s[0])*60+(+s[1])>=(+e[0])*60+(+e[1])){
-          error=true;
-          const tar=doc.getElementById("div_"+key).getElementsByTagName("input");
-          tar[i*3+1].style.backgroundColor="rgba(255,0,0,0.5)";
-          tar[i*3+2].style.backgroundColor="rgba(255,0,0,0.5)";
+  window.loadEnd=nenndo=>{
+    const doc=document;
+    const db=firebase.firestore().doc("/kaedefestival/kaede"+nenndo),
+      log=doc.getElementById("log");
+    log.textContent="loadend";
+    doc.getElementById("upload").addEventListener("click",()=>{
+      log.textContent="実行中";
+      let error=false;
+      const doError=(key,i)=>{
+        error=true;
+        doc.getElementById("div_"+key).getElementsByTagName("input")[i].style.backgroundColor="rgba(255,0,0,0.5)";
+      };
+      for(const key in datas){
+        const data=datas[key],l=data.length;
+        for(let i=0;i<l;i++){
+          const v=data[i];
+          if(!v[0])doError(key,i*3);
+          if(!timeReg.test(v[1]))doError(key,i*3+1);
+          if(!timeReg.test(v[2]))doError(key,i*3+2);
+          const s=v[1].split(":"),e=v[2].split(":");
+          if(s[0]>24||s[1]>59)doError(key,i*3+1);
+          if(e[0]>24||e[1]>59)doError(key,i*3+2);
+          if((+s[0])*60+(+s[1])>=(+e[0])*60+(+e[1])){
+            error=true;
+            const tar=doc.getElementById("div_"+key).getElementsByTagName("input");
+            tar[i*3+1].style.backgroundColor="rgba(255,0,0,0.5)";
+            tar[i*3+2].style.backgroundColor="rgba(255,0,0,0.5)";
+          }
         }
       }
-    }
-    if(error)return;
-    const batch=firebase.firestore().batch();
-    const cols={};
-    (async()=>
-      await Promise.all(
-        Object.keys(datas).map(async key=>{
-          const dbRef=db.collection("/scheduleTest/datas"+key),
-            data=datas[key];
-          const docSnps=await Promise.all(data.map(d=>dbRef.where("name","==",d[0]).get()));
-          docSnps.forEach((docSnp,i)=>{
-            const d=data[i];
-            if(docSnp.empty)
+      if(error){
+        log.textContent="エラー";
+        return;
+      }
+      (async()=>{
+        const batch=firebase.firestore().batch();
+        const docs={};
+        const timeRef=db.collection("timeTest");//本番で変更
+        const promiseArr=[];
+        for(const key in datas)
+          datas[key].forEach(d=>{
+
+          });
+        Promise.all(
+          Object.keys(datas).map(key=>{
+            const dbRef=db.collection("scheduleTest/datas/"+key);//本番で変更
+            datas[key].forEach(async d=>{
+              const end=+d[2].split(":")[0];
+              for(let hour=+d[1].split(":")[0];hour<=end;hour++){
+                let doc=[];
+                if(docs[hour])
+                  doc=docs[hour];
+                else{
+                  doc=await timeRef.add({hour:hour});
+                  docs[hour]=doc;
+                }
+                batch.set(doc.collection("datas").doc(),{name:d[0],start:d[1],end:d[2],place:key});
+              }
               batch.set(dbRef.doc(),{name:d[0],start:d[1],end:d[2]});
-            else
-              batch.update(docSnp.docs[0].ref,{start:d[1],end:d[2]});
-          });
-          const timeRef=db.collection("timeTest");
-          data.forEach(d=>{
-            const hour=d[1].split(":")[0];
-            let col;
-            if(cols[hour])
-              col=cols[hour];
-            else{
-              col=await timeRef.where("hour","==",d[0].split(":")[0]).get();
-              cols[hour]=col;
-            }
-            col.docs[0].collection("datas");
-          });
-        })
-      )
-    )().then(()=>
-      batch.commit()
-    ).then(res=>{
-        console.log(res);
-    }).catch(E=>console.log(E));
-  });
+            });
+          })
+        ).then(()=>
+          batch.commit()
+        ).then(res=>
+          log.textContent="更新完了"
+        ).catch(E=>
+          log.textContent=E
+        );
+      })();
+    });
+  };
 })();
